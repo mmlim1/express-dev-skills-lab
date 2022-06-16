266b5212d216434bbbe49beefdd80aeabba895d1
mmlim1 <michaelmlim1@gmail.com>
2022-06-16 14:22:08 -0700

    complete step 2

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,9 +1,24 @@
-import { basketballTeams } from "../data/skill-data.js";
+// import { Skill } from "../models/skills.js";
+import { basketballTeams } from "../data/skill-data.js"
 
-function index (req, res) {
-  res.render('skills/index', {
-    skills: basketballTeams
-  })
+// function index (req, res) {
+//   Skill.find({})
+//   res.render('skills/index', {
+//     skills: basketballTeams
+//   })
+// }
+
+function index(req, res) {
+  // basketballTeams.find({})
+  // .then(skills => {
+    res.render('skills/index', {
+      skills: basketballTeams,
+    })
+  // })
+  // .catch(error => {
+    // console.log(error)
+    // res.redirect('/')
+  // })
 }
 
 export {
